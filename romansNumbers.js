22cd0a8704fa9a276eb64a4fb3c11459fdd97789
alsdeok <axel.s09kpo@gmail.com>
2023-03-22 16:11:51 -0300

    romansNumbers.js simplificado mediante el uso de objeto literal

--- romansNumbers.js
+++ romansNumbers.js
@@ -20,7 +20,20 @@
 // All roman numerals answers should be provided in upper-case.
 
 
+romanize(500);
 
+function romanize(num) {
+  var lookup = {M:1000,CM:900,D:500,CD:400,C:100,XC:90,L:50,XL:40,X:10,IX:9,V:5,IV:4,I:1},
+      roman = '' ;
+  for ( let i in lookup ) {
+    while ( num >= lookup[i] ) {
+    console.log(lookup[i]);
+      roman += i;
+      num -= lookup[i];
+    }
+  }
+ return roman;
+}
 /////////////////////////////////////////////////////////////////////////
 
 function convertToRoman(num) {
